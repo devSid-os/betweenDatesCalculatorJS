b4abc1cebaa2f4633c64e62d92b749fef0165f01
devSid-os <siddjain341@gmail.com>
2024-01-26 19:48:54 +0530

    Refactored js code for validating date

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const toMonth = document.getElementById("toMonth");
 const toYear = document.getElementById("toYear");
 const toError = document.getElementById("toError");
 
+const cError = document.getElementById("cError");
+
 const date = new Date();
 
 fromDay.setAttribute("min", 1);
@@ -28,45 +30,53 @@ toYear.value = date.getFullYear();
 
 function isLeapYear(year) {
     if (year % 4 === 0) {
-        if (year % 100 === 0) {
-            return (year % 400) === 0;
-        }
+        if (year % 100 === 0) return (year % 400) === 0;
         return true;
     }
     return false;
 }
 
-function setError(type) {
-    if (type === 'f') fromError.classList.remove("hidden");
+function setError(prefix) {
+    if (prefix === 'from') fromError.classList.remove("hidden");
     else toError.classList.remove("hidden");
 }
 
-function removeError(type) {
-    if (type === 'f') fromError.classList.add("hidden");
+function removeError(prefix) {
+    if (prefix === 'from') fromError.classList.add("hidden");
     else toError.classList.add("hidden");
 }
 
-function checkDay(e, type) {
-    if (!e.value) {
-        if (type === 'f') fromDay.value = date.getDate();
+function compareDates() {
+    const fromDate = new Date(`${fromYear.value}-${fromMonth.value}-${fromDay.value}`);
+    const toDate = new Date(`${toYear.value}-${toMonth.value}-${toDay.value}`);
+    if (fromDate.getTime() > toDate.getTime()) cError.classList.remove("hidden");
+    else cError.classList.add("hidden");
+}
+
+function checkDay(prefix) {
+    const day = document.getElementById(`${prefix}Day`).value;
+    const month = document.getElementById(`${prefix}Month`).value;
+    if (!day) {
+        if (type === 'from') fromDay.value = date.getDate();
         else toDay.value = date.getDate();
         return;
     }
-    if (e.value < 1) setError(type);
-    else {
-        if (e.value > 31) setError(type);
-        else removeError(type);
-    }
+    if (day < 1 || day > 31 || month < 1 || month > 12) setError(prefix);
+    else removeError(prefix);
+
+    compareDates();
 }
-function checkMonth(e, type) {
-    if (!e.value) {
-        if (type === 'f') fromMonth.value = date.getMonth() + 1;
+function checkMonth(prefix) {
+    const day = document.getElementById(`${prefix}Day`).value;
+    const month = document.getElementById(`${prefix}Month`).value;
+    if (!month) {
+        if (type === 'from') fromMonth.value = date.getMonth() + 1;
         else toMonth.value = date.getMonth() + 1;
         return;
     }
-    if (e.value < 0) setError(type);
-    else {
-        if (e.value > 12) setError(type);
-        else removeError(type);
-    }
+    if (day < 1 || day > 31 || month < 1 || month > 12) setError(prefix);
+    else removeError(prefix);
+
+    compareDates();
 }
+
